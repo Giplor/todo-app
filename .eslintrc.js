25f84948fa840abcc2d2237fcf98614f6ce24802
Giplor <m.lysenko.w@gmail.com>
2023-07-02 12:55:46 +0300

    refactor: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,6 @@
 module.exports = {
   extends: ['universe', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        arrowParens: 'always',
-        bracketSameLine: false,
-        bracketSpacing: true,
-        singleQuote: true,
-        trailingComma: 'all',
-        printWidth: 100,
-        endOfLine: 'auto',
-      },
-    ],
+    'prettier/prettier': 'error',
   },
 };
